667a26320ac6ba0ac304ae641c1f0513de8505d0
tomerurman <59766452+tomerurman@users.noreply.github.com>
2020-09-05 15:49:54 +0300

    Setup project
    Signed-off-by: tomerurman <59766452+tomerurman@users.noreply.github.com>

---
+++ app/src/main/java/com/example/tomer_and_miki/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tomer_and_miki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
